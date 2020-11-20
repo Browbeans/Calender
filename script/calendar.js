09b923fdd2603deff92f3ccf9fc3fbc2804781fe
stanggren <astanggren@gmail.com>
2020-11-20 14:57:34 +0100

    dynamic rendering of calendar days

--- script/calendar.js
+++ script/calendar.js
@@ -1,9 +1,50 @@
-function getDaysInMonth(month, year) {
+let dates = [];
+let days = [];
+let calendarContainer = document.getElementById("calendar-container");
+
+function getDatesInMonth(month, year) {
   let date = new Date(year, month, 1);
-  let days = [];
   while (date.getMonth() === month) {
-    days.push(new Date(date));
+    dates.push(new Date(date));
     date.setDate(date.getDate() + 1);
+  }
+  console.log(dates);
+}
+
+function getDaysInMonth() {
+  for(let i = 0; i < dates.length; i++){
+    separateDate(dates[i]);
   }
   console.log(days);
 }
+
+function separateDate(date) {
+  day = date.toString().split(" ");
+  days.push(day[2]);
+}
+
+
+function renderDays(days){
+  for (let i = 0; i < days.length; i++){
+    let parent = createHTMLElement('div');
+    let date = createHTMLElement('h3');
+
+    setHTMLContent(parent, null);
+    setHTMLContent(date, days[i]);
+
+    parentClasses = ["calender-day", "flex"];
+    setHTMLClass(parent, parentClasses);
+    setHTMLClass(date, "calender-date");
+
+
+    setDayHTMLStructure(parent, date);
+  }
+}
+
+function setDayHTMLStructure(parent, date){
+  calendarContainer.appendChild(parent);
+  parent.appendChild(date);
+}
+
+
+
